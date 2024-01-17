9558e74c7c9425544af0486e0689d542a0641f0c
Malgorzata Librant <librantgosia@gmail.com>
2024-01-17 17:06:58 +0100

    code formatting

--- js/script.js
+++ js/script.js
@@ -1,39 +1,40 @@
 {
-const calculateResult = (amount, currency) => {
+  const calculateResult = (amount, currency) => {
     const rateEUR = 4.6981;
     const rateGBP = 5.3561;
 
     switch (currency) {
-        case "EUR":
-            return amount / rateEUR;
+      case "EUR":
+        return amount / rateEUR;
 
-        case "GBP":
-            return amount / rateGBP;
+      case "GBP":
+        return amount / rateGBP;
     }
-};
+  };
 
-const updateResultText = (amount, result, currency) => {
+  const updateResultText = (amount, result, currency) => {
     const resultElement = document.querySelector(".js-result");
-    resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
-}
-const onFormSubmit = (event) => {
+    resultElement.innerHTML = `${amount.toFixed(
+      2
+    )} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+  };
+  const onFormSubmit = (event) => {
     event.preventDefault();
 
-const amountElement = document.querySelector(".js-amount");
-const currencyElement = document.querySelector(".js-currency");
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
 
     const amount = +amountElement.value;
     const currency = currencyElement.value;
 
-    const result = calculateResult (amount, currency);
+    const result = calculateResult(amount, currency);
     updateResultText(amount, result, currency);
-    
-};
+  };
 
-   const init = () => {
+  const init = () => {
     const formElement = document.querySelector(".js-form");
-    formElement.addEventListener("submit", onFormSubmit);  
-    };
+    formElement.addEventListener("submit", onFormSubmit);
+  };
 
-   init ();
+  init();
 }
